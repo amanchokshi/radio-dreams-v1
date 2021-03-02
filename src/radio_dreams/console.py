f188378335a3574f1bf891999bbb26adb3695a70
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-03-03 02:15:33 +1100

    Added plot-array console script

--- src/radio_dreams/console.py
+++ src/radio_dreams/console.py
@@ -1,9 +1,11 @@
 import click
+from matplotlib import pyplot as plt
+
+from radio_dreams import interferometer
 
 from . import __version__
 
 
-@click.command()
 @click.version_option(version=__version__)
 def main():
     """The Radio Dreams Python project."""
@@ -14,3 +16,26 @@ def main():
     click.echo(f"V:{__version__}\n")
 
     click.echo("Imagined Interferometers")
+
+
+@click.command()
+@click.option(
+    "--array", prompt="Path to array csv file", help="Path to array layout csv file"
+)
+@click.option("--latitude", prompt="Array latitude", help="Latitude of array")
+def plot_array(array, latitude):
+    """Plot the layout antennas for an interferometer"""
+    mwa = interferometer.ArrayConfig(array_csv=array, latitude=latitude)
+
+    plt.style.use("seaborn")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(mwa.east, mwa.north, marker="s", color="seagreen")
+    ax.set_aspect(aspect=1)
+    ax.set_xlabel("East [m]")
+    ax.set_ylabel("North [m]")
+    ax.set_title("Array layout")
+
+    plt.tight_layout()
+    plt.show()
