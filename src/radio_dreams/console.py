0b0182a73831b6133aef7a4a984e247fba92dea3
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-03-04 00:45:26 +1100

    Save array to png

--- src/radio_dreams/console.py
+++ src/radio_dreams/console.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 from matplotlib import pyplot as plt
 
@@ -18,23 +20,32 @@ def radio_dreams():
 
 
 @click.command()
+@click.option("--array_csv", help="Path to array layout csv file", type=str, required=True)
 @click.option(
-    "--array", prompt="Path to array csv file", help="Path to array layout csv file"
+    "--out_dir",
+    help="Output directory to save plots",
+    type=str,
+    default="./dream-plots",
+    show_default=True,
 )
-@click.option("--latitude", prompt="Array latitude", help="Latitude of array")
-def plot_array(array, latitude):
+def plot_array(array_csv, out_dir):
     """Plot the layout antennas for an interferometer"""
-    mwa = interferometer.ArrayConfig(array_csv=array, latitude=latitude)
+
+    array_csv = Path(array_csv)
+    mwa = interferometer.ArrayConfig(array_csv=array_csv)
+
+    # Make out_dir if doesn't exist
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     plt.style.use("seaborn")
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(mwa.east, mwa.north, marker="s", color="seagreen")
+    ax.scatter(mwa.east, mwa.north, marker="s", color="seagreen", alpha=0.8)
     ax.set_aspect(aspect=1)
     ax.set_xlabel("East [m]")
     ax.set_ylabel("North [m]")
-    ax.set_title("Array layout")
+    ax.set_title(f"{array_csv.stem} array layout")
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig(f"{out_dir}/{array_csv.stem}_array.png", dpi=300)
