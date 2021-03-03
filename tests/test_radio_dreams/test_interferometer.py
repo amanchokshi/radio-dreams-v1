f9b00f2bc3ea482bd0273ba88a3176cac9b7f931
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-03-04 00:06:12 +1100

    New test dir tree, sample test array data

---
+++ tests/test_radio_dreams/test_interferometer.py
@@ -0,0 +1,25 @@
+from os import path
+
+from radio_dreams.interferometer import ArrayConfig
+
+# Save the path to this directory
+dirpath = path.dirname(__file__)
+
+# Obtain path to directory with test_data
+test_data = path.abspath(path.join(dirpath, "../test_data"))
+
+
+def test_ArrayConfig():
+    mwa = ArrayConfig(array_csv=f"{test_data}/test_mwa.csv", latitude=-27)
+    x, y, z = mwa.enh_xyz()
+
+    # Test __init__
+    assert mwa.east[0] == -1999.81
+    assert mwa.north[0] == 206.85
+    assert mwa.height[0] == 372.921
+    assert mwa.tiles[0] == "LBA1"
+
+    # Test enh_xyz
+    assert x[0] == 88.88166409864053
+    assert y[0] == -1999.81
+    assert z[0] == -554.4790283870071
